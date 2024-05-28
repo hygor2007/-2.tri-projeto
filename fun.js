f14f32005f369330df0ac421cf50b54f59c5a24b
hygor2007 <129951351+hygor2007@users.noreply.github.com>
2024-05-28 09:47:02 -0300

    Create fun.js

---
+++ fun.js
@@ -0,0 +1,23 @@
+console.log(soma (5,9));
+function soma(v1=0,v2=0){
+    return v1 + v2;
+}
+
+function nomeidade(nome="", idade=""){
+    console.log(`nome:${nome} - idade:${idade}`);
+}
+nomeidade("hygor","17");
+nomeidade("22", "hygor");
+console.log(soma(1));
+nomeidade("oliveira");
+
+
+
+
+
+function multiplicacao(v1=0, v2=0){
+   return v1 + v2
+}
+console.log(multiplicacao(1358,86));
+multiplicacao(soma(3,5), soma(8,7));
+multiplicacao(5);
