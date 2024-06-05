797a8a82725a7c9db74387da55acba30bbea3072
hygor2007 <129951351+hygor2007@users.noreply.github.com>
2024-06-05 09:02:46 -0300

    Create lista.js

---
+++ lista.js
@@ -0,0 +1,5 @@
+const produtos = ["cloro","amaciante","detergente","sabão em po","sabão","sabão liquido","sabonete","sabonete liquido","sabão em pedra","agua sanitaria","pasta de dente","esponja","alcool perfumado","alcool"]
+console.log(produtos);
+console.log(produtos[2]);
+console.log(produtos.length);
+console.log()
