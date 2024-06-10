c55e456d7d5a9e5c2cd293dfc68fa1b63fef24c3
hygor2007 <129951351+hygor2007@users.noreply.github.com>
2024-06-10 12:35:10 -0300

    Update lista.js

--- lista.js
+++ lista.js
@@ -2,4 +2,19 @@ const produtos = ["cloro","amaciante","detergente","sabão em po","sabão","sab�
 console.log(produtos);
 console.log(produtos[2]);
 console.log(produtos.length);
-console.log()
+
+
+produtos.push("cloro")
+produtos.pop( )
+nprodutos = produtos.splice(3,4)
+
+console.log(nprodutos)
+produtos1 = produtos.slice(0,5)
+produtos2 = produtos.slice(5)
+console.log(produtos1)
+console.log(produtos2)
+
+produtos1 push("")
+produtos2 push ("")
+
+listacompleta = produtos1.concat(list22)
