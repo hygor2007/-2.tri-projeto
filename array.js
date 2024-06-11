cda87ae1475bbc44aef73cbc9af4be7301ea1d09
hygor2007 <129951351+hygor2007@users.noreply.github.com>
2024-06-11 09:00:23 -0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,10 @@
+lista1 = ["leandro","maria","monica"]
+lista2 = ["fassina","silva","fabio"]
+listacompleta = lista1.concat(lista2)
+console.log (listacompleta)
+
+const dado = [["marcos","rafaela","maria"],[8 , 8.5 , 9]];
+console.log("nome: " + dado[0][1] + " média: " + dado [1][1]);
+
+const dado2 = [["marcos","rafaela","maria"],[[3, 9, 7],[5, 8, 9][8, 9, 7]]];
+console.log ("nome: " + dado2 [0][1] + " média: " + dado2 [1][1][0]);
