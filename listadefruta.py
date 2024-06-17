745d0e0b31b0ef5c1d097e724d54d292e3680b10
hygor2007 <129951351+hygor2007@users.noreply.github.com>
2024-06-17 08:53:52 -0300

    Create listadefruta.py

---
+++ listadefruta.py
@@ -0,0 +1,11 @@
+listafrutas = ["morango", "manga", "mamão"]
+fruta = "marango"
+print (listafrutas[1])
+print(fruta[0])
+print(listafrutas[2][0])
+listafrutas.append ("abacaxi")
+print(listafrutas) 
+listafrutas.remove("mamão")
+print(listafrutas)
+listafrutas.sort()
+print(listafrutas)
