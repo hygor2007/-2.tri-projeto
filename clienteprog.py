b25b08fac254bd16b92dadd00d1748fe451ae65f
hygor2007 <129951351+hygor2007@users.noreply.github.com>
2024-07-31 12:31:22 -0300

    Update clienteprog.py

--- clienteprog.py
+++ clienteprog.py
@@ -18,3 +18,28 @@ print("propriedade nome do cliente:", Cliente.nome)
 ##usando a propriedade setter
 Cliente.nome = 'arisberto'
 print("propriedade nome do cliente", Cliente.nome )
+
+----------------------------------
+cliente.py
+class Cliente:
+    def __init__(self, nome):
+        self.__nome = nome
+
+
+    ## MEDODO
+   
+    def get_nome(self):
+        return self.__nome ##.title()
+    
+    
+    def set_nome(self,nome):
+        self.__nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome.title()
+    
+    ## propriedade setter
+    @nome.setter
+    def nome(self,nome):
+        self.__nome = nome 
